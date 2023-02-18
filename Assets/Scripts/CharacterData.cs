bd86575a51545c060229bd87866c351aacc2064c
Anthony4pf <edeanthony14@gmail.com>
2023-02-17 16:00:41 -0800

    Add Project Files

---
+++ Assets/Scripts/CharacterData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Character", menuName = "Character")]
+public class CharacterData : ScriptableObject
+{
+    public new string name;
+    public int health;
+    public float speed;
+    public float attackPoints;
+    public Sprite image;
+    public GameObject player;
+}
---
+++ Assets/Scripts/CharacterSelector.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterSelector : MonoBehaviour
+{
+     //private Vector3 spawnPos = new Vector3(0, 1, -5);
+     public Text playerName;
+     public Text health;
+     public GameObject characterSelectPanel;
+     public GameObject gamePanel;
+     public GameObject player;
+     public GameObject playerClone;
+     public CharacterData[] characters;
+     public void StartGame(int characterChoice)
+     {
+          CharacterData selectedCharacter = characters[characterChoice];
+          playerName.text = selectedCharacter.name.ToString();
+          health.text = selectedCharacter.health.ToString();
+          characterSelectPanel.SetActive(false);
+          gamePanel.SetActive(true);
+          player = selectedCharacter.player;
+          playerClone = Instantiate(player, player.transform);
+     }
+     public void Cancel()
+     {
+          characterSelectPanel.SetActive(true);
+          gamePanel.SetActive(false);
+          Destroy(playerClone);
+     }
+}
